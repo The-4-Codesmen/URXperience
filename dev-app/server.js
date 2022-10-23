af73b907c53e2989cbf73e399f917e4c9cb43853
Dhruv Modi <dhruvkmodi@gmail.com>
2022-10-22 20:47:40 -0600

    created a new branch

--- dev-app/server.js
+++ dev-app/server.js
@@ -1,55 +1,51 @@
-const express = require('express')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const connectDB = require('./config/db')
-const app = express()
-
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const app = express();
 
 //Config.env to ./config/config.env
-require('dotenv').config({
-    path: './config/config.env'
-})
+require("dotenv").config({
+  path: "./config/config.env",
+});
 
-//connect to Database 
-connectDB()
-
+//connect to Database
+connectDB();
 
 //Use bodyParser
-app.use(bodyParser.json())
-// comment
-//comment 2
+app.use(bodyParser.json());
 
 //Load all routes
-const authRouter = require('./routes/authroute')
+const authRouter = require("./routes/authroute");
 // Config for only development
-if(process.env.NODE_ENV === 'development'){
-    app.use(cors({
-        origin: process.env.CLIENT_URL
-    }))
+if (process.env.NODE_ENV === "development") {
+  app.use(
+    cors({
+      origin: process.env.CLIENT_URL,
+    })
+  );
 
-    app.use(morgan('dev'))
+  app.use(morgan("dev"));
 
-    // Morgan gives info about each request
-    // Cors it allows to deal with react for localhost at port 3000
-    // without much problem
+  // Morgan gives info about each request
+  // Cors it allows to deal with react for localhost at port 3000
+  // without much problem
 }
 
-
-
 //Using routes
-app.use('/api', authRouter);
+app.use("/api", authRouter);
 
-app.use((req, res, next) =>{
-    res.status(404).json({
-        sucess: false,
-        message:"Page not found"
-    })
-})
+app.use((req, res, next) => {
+  res.status(404).json({
+    sucess: false,
+    message: "Page not found",
+  });
+});
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 //port for server side
-app.listen(PORT,()=>{
-    console.log('listening on port '+PORT);
-})
+app.listen(PORT, () => {
+  console.log("listening on port " + PORT);
+});

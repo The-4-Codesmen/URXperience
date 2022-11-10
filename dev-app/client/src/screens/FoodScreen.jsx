eeb9bd94d43f304e41e0dfe6f035d84ce70f0062
AbrahamMugerwa <abraham.mugerwa@gmail.com>
2022-11-10 16:40:30 -0600

    Update Foodscreen

--- dev-app/client/src/screens/FoodScreen.jsx
+++ dev-app/client/src/screens/FoodScreen.jsx
@@ -8,13 +8,45 @@ import Navbar from './Navbar';
 import Dashboard from './Dashboard';
 const FoodScreen = () => {
  
-  return (
-    
-    <div className='min-h-screen bg gray-100 text-gray-900 flex justify-center'>
-      Hello World
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = getCookie('token')
+    if (!getCookie('token')) {
+        navigate("/login");
+    }else{
+        axios.get(`http://localhost:5000/api/user/${isAuth()._id}`,{
+          headers:{
+            Authorization: `Bearer ${token}`
+          }
+        }).then(res=>{
+          const {role, name, email} = res.data
+        }).catch(err=>{
+          toast.error(`Error to your Information ${err.response.statusText}`)
+          if(err.response.status === 401){
+            signout(()=>{
+              navigate('/login')
+            })
+          }
+        })
+      }
+}, []);
 
-    </div>
-  );
+return (
+  <div className='min-h-screen bg gray-100 text-gray-900 flex justify-center'>
+      <Navbar />
+      <ToastContainer/> 
+
+  <div className='min-h-screen bg gray-100 text-gray-900 flex justify-center'>Hello World</div>
+
+  </div>
+
+);
+
+
+
+
+
+
 };
 
 export default FoodScreen

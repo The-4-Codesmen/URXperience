4a5b69c0071063db38a87a34d294a7e683ec5716
Dhruv Modi <dhruvkmodi@gmail.com>
2022-11-12 12:27:10 -0700

    Updated FoodScreen - Resolved Merge Conflicts

--- dev-app/client/src/screens/FoodScreen.jsx
+++ dev-app/client/src/screens/FoodScreen.jsx
@@ -6,38 +6,15 @@ import { useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import App from "../App";
 
-const FoodScreen = () => {
-  const [meals, setMeals] = useState([]);
-  useEffect(() => {
-    axios
-      .get("https://www.themealdb.com/api/json/v1/1/random.php")
-      .then(({ data }) => setMeals(data.meals))
-      .catch((error) => console.log(error));
-  }, []);
+import Pages from "./FoodPages/Pages";
 
+function FoodScreen() {
   return (
-    <div className="min-h-screen bg gray-100 text-gray-900 flex justify-center">
+    <div className="min-h- bg gray-100 text-gray-900 flex grid justify-center">
       <h1>Recipe Of The Day</h1>
-
-      <div>
-        <br></br>
-        {meals.map((meal) => (
-          <div key={meal.idMeal}>
-            {meal.strMeal}
-
-            <Card style={{ width: "18ren" }}>
-              <Card.Img variant="top" src={meal.strMealThumb} width="200px" />
-              <Card.Body>
-                <Card.Title src={meal.idMeal}></Card.Title>
-                <Card.Text>Some quick test example</Card.Text>
-                <Button variant="primary">Show Details</Button>
-              </Card.Body>
-            </Card>
-          </div>
-        ))}
-      </div>
+      <Pages />
     </div>
   );
-};
+}
 
 export default FoodScreen;

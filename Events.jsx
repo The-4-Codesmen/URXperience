ce8f3a4617c1e77bff0350259d0c14fc70e2f0b3
Dineshd20 <90287592+Dineshd20@users.noreply.github.com>
2023-01-26 16:19:11 -0600

    Add files via upload

---
+++ Events.jsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react";
+import Navbar from "./Navbar";
+
+
+import EventPages from "./EventComponents/EventPages";
+
+import axios from "axios";
+import EventBusyTwoToneIcon from '@mui/icons-material/EventBusyTwoTone';
+import { purple } from '@mui/material/colors';
+
+function Events() {
+
+  const [allEvents, setAllEvents]= useState([]);
+
+
+  
+
+   const getAllEvents = async() =>{
+
+
+ 
+    axios.get(`http://localhost:5000/api/eventfindall`)
+    .then(res => {
+      console.log(res.data)
+      setAllEvents(res.data)
+    })
+    .catch(err => {
+     console.log(err)
+     })
+ //    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-5">
+
+   };
+ 
+  useEffect(()=>{
+
+    getAllEvents();
+
+    },[   getAllEvents() ]);
+
+
+  return (
+    <div className='min-h-screen  text-gray-900 flex justify-center mt-20'>
+      <Navbar />
+      
+    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+    
+ 
+    <EventPages />
+     
+     <div className="col-span-2 justify-center ">
+    
+      <h1 className='text-2xl xl:text-3xl font-extrabold text-center '>ALL EVENTS</h1>
+
+    
+       <div className="mt-2 grid p-4 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-8 auto-cols-auto"> 
+
+      { allEvents.map((event)=>(
+            <div className="bg-gray-100 rounded-lg p-3 shadow-lg items-center" key={event.id}>
+                      
+                    <div className='rounded-lg'>
+                        
+                      <h1 className="font-mono font-bold text-indigo-500 text-2xl">{event.title}</h1> <br></br>
+                      <label>Description:</label>
+                      <h3>{event.description}</h3><br></br>
+
+
+                      <EventBusyTwoToneIcon className=" flex-right leading-tight relative " color="action " fontSize="large"  sx={{color:purple[900]}} ></EventBusyTwoToneIcon>
+
+                      <p className=" ">From: {event.from} To: {event.to}</p>
+                    
+                      <p>{event.date}</p><br></br>
+                    
+                    </div>
+                    
+            </div>
+
+      ))}
+   
+    </div>
+    </div>
+
+    </div>
+
+     
+     
+    
+
+
+
+    </div>
+
+  );
+}
+
+export default Events;

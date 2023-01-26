67dee4097c440114426b6c6d305ae9218e262a7e
AbrahamMugerwa <abraham.mugerwa@gmail.com>
2023-01-25 23:23:03 -0600

    recipe search feature added for mvp2

--- dev-app/client/src/screens/FoodComponents/Search.jsx
+++ dev-app/client/src/screens/FoodComponents/Search.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect, useState } from "react";
-import {searchIcon} from 'react-icons/fa';
+import {FaSearch} from 'react-icons/fa';
+import { useParams } from 'react-router-dom';
 
 
 
@@ -8,21 +9,53 @@ function Search() {
 
 
     const [input, setInput] = useState("");
-    const submitHandler =(e)=>{
+    
+    const submitHandler = (e) => {
+      e.preventDefault();
+      //console.log("test")
+      
+    };
+
+    let params = useParams()
+
+    const FOOD_API_KEY = '49198b4329a048b381f314c4141e6c14';
+
+    const getInput = async(name)=> {
+      const api = await fetch('https://api.spoonacular.com/recipes/findByIngredients?apiKey=${FOOD_API_KEY}&number=1&instructionsRequired=true&ingredients=${name}')
+      const data = await api.json();
+      setInput(data.recipes);
 
 
-        e.preventDefault();
-    }
+    };
+
+    useEffect(() =>{
+    
+     console.log(params.type)
+    },[params.type]);
+   
+    
 
   return (
-    <div>
-        <form>
-        <searchIcon/>
-        <input value ={input} type= "text"/>
+    <>
+         <form onSubmit={submitHandler}> 
+          <div>
+        <FaSearch></FaSearch>
+        <input 
+        onChange={(e) => setInput(e.target.value)}
+        type = "text"
+        value={input}
+        />
 
-        </form>
-    </div>
-  )
+          </div>
+        </form> 
+
+
+
+
+
+    
+    </>
+  );
 }
 
 
